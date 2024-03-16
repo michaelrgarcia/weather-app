ec74aeec3c6c187a8edf44307529a16cbd24feae
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-03-16 12:05:45 -0700

    Rename to pageTransitions

---
+++ src/pageTransitions.js
@@ -0,0 +1,15 @@
+const input = document.querySelector("input");
+const errorLabel = document.querySelector("span.error");
+
+export default function checkErrors() {
+  if (input.validity.valueMissing) {
+    input.classList.add("no-val");
+    errorLabel.textContent = "This field cannot be empty.";
+  } else {
+    errorLabel.textContent = "";
+    input.classList.remove("no-val");
+    return false;
+  }
+
+  return true;
+}
