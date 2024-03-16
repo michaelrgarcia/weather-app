f04dd89e84bd701d7120a857932d95a08ba2f52d
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-03-16 12:03:42 -0700

    Move checkErrors from userInput module

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+import getWeatherInfo from "./apiHitter";
 import logo from "./imgs/logo.png";
-import checkErrors from "./userInput";
 
 const input = document.querySelector("input");
 const submitBtn = document.getElementById("submit");
+const errorLabel = document.querySelector("span.error");
 
 function loadImgs() {
   const logoImg = document.querySelector("img");
 
   logoImg.src = logo;
+}
+
+function checkErrors() {
+  if (input.validity.valueMissing) {
+    input.classList.add("no-val");
+    errorLabel.textContent = "This field cannot be empty.";
+  } else {
+    errorLabel.textContent = "";
+    input.classList.remove("no-val");
+    return false;
+  }
+
+  return true;
 }
 
 loadImgs();
@@ -20,6 +34,6 @@ submitBtn.addEventListener("click", () => {
   const inputError = checkErrors();
 
   if (!inputError) {
-    console.log("hi");
+    getWeatherInfo(input.value);
   }
 });
